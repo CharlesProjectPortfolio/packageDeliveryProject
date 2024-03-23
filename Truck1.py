f768861fbb37054fc6f4972778f53079dcae135a
Charles Chrietzberg <cchriet@wgu.edu>
2024-03-22 21:06:10 -0500

    Cleaned up the code some more

--- Truck1.py
+++ Truck1.py
@@ -21,6 +21,7 @@ def delivery(address, truck1, distancesList, truckSpeed, currentTimeTruck1, tota
             countInd = countInd + 1
         countInd = 0
 
+    #build a list of delivery deadlines for the truck
     build = ""
     buildList = list()
     testing = str(datetime.date.today()) + " 12:00:00"
@@ -30,7 +31,6 @@ def delivery(address, truck1, distancesList, truckSpeed, currentTimeTruck1, tota
             build = str(datetime.date.today()) + " 12:00:00"
             buildList.append(datetime.datetime.strptime(build, '%Y-%m-%d %H:%M:%S'))
             build = ""
-
         else:
             for j in i.deliveryDeadline:
                 if j == " ":
@@ -41,58 +41,40 @@ def delivery(address, truck1, distancesList, truckSpeed, currentTimeTruck1, tota
                     build = ""
                     break
                 build = build + j
-            #print(i.deliveryDeadline)
 
     # while loop is O(n^3)
     while addressIndexTruck1:  # While loop to iterate through truck 1 and deliver the packages to the next closest address
+        # Loop through a list of index positions from address
+        # Use the index to get the row in distancesList
+        # Since addressList and DistancesList are both ordered the index from addressList
+        # will match up to the distance in that row of distancesList
+        # If they match put the distance into distancesAddressIndexTruck1
         if moving == 0:  # If first iteration of the while loop get the closest address to the HUB
-            for i in addressIndexTruck1:  # Loop through a list of index positions from address
+            for i in addressIndexTruck1:
                 for j in range(
-                        len(distancesList[moving])):  # Use the index to get the row in distancesList
-                    # Since addressList and DistancesList are both ordered the index from addressList
-                    # will match up to the distance in that row of distancesList
+                        len(distancesList[moving])):
                     if j == i:  # Check if j and i math index values
-                        # If they match put the distance into distancesAddressIndexTruck1
                         distancesAddressIndexTruck1.append(distancesList[moving][j])
                         break
-            '''
-            for i in truck1:
-                if i.ID != 15:
-                    continue
-                for j in range(len(addressIndexTruck1)):
-                    if i.address == address[addressIndexTruck1[j]]:
-                        print(i.deliveryDeadline)
-                        pHolder = j
-                        break
-                        #placeHolder.append(j)
-            
-            # force the delivery of package number 15 to meet the 9 AM deadline
-            milesTraveled = distancesAddressIndexTruck1[pHolder]  # Add the distance traveled to the next address to milesTraveled
-            totalMiles = totalMiles + milesTraveled  # variable for total miles traveled
-            nextIndexForDistances = addressIndexTruck1[distancesAddressIndexTruck1.index(milesTraveled)]  # Use that index to get the value for the next distance list
-            '''
         else:
             for i in addressIndexTruck1:
                 for j in range(len(distancesList[nextIndexForDistances])):
                     if j == i:
                         distancesAddressIndexTruck1.append(distancesList[nextIndexForDistances][j])
                         break
-
+        # check for any packages with a delivery deadline within 20 minutes
         checking = currentTimeTruck1 + datetime.timedelta(minutes = 20)
         if checking >= min(buildList):
-            print("IN THE IF STATEMENT")
             if distancesAddressIndexTruck1.index(min(distancesAddressIndexTruck1)) == buildList.index(min(buildList)):
                 milesTraveled = min(distancesAddressIndexTruck1)  # Add the distance traveled to the next address to milesTraveled
                 totalMiles = totalMiles + milesTraveled  # Variable for total miles traveled
                 nextIndexForDistances = addressIndexTruck1[distancesAddressIndexTruck1.index(milesTraveled)]  # Use that index to get the value for the next distance list
-
             else:
                 hold = buildList.index(min(buildList))
                 milesTraveled = distancesAddressIndexTruck1[hold]
                 totalMiles = totalMiles + milesTraveled
                 nextIndexForDistances = addressIndexTruck1[distancesAddressIndexTruck1.index(milesTraveled)]
         else:
-            print("IN THE ELSE STATEMENT")
             milesTraveled = min(distancesAddressIndexTruck1)
             totalMiles = totalMiles + milesTraveled
             nextIndexForDistances = addressIndexTruck1[distancesAddressIndexTruck1.index(milesTraveled)]
@@ -106,7 +88,6 @@ def delivery(address, truck1, distancesList, truckSpeed, currentTimeTruck1, tota
             math.floor((min(distancesAddressIndexTruck1) / truckSpeed)))) * 60)
         currentTimeTruck1 = currentTimeTruck1 + datetime.timedelta(minutes=minutes,
                                                                                  seconds=seconds)
-        print(milesTraveled, "   ", currentTimeTruck1)
         while nextIndexForDistances in addressIndexTruck1:  # remove values for already delivered packages
             for i in truck1:
                 if i.address == address[nextIndexForDistances]:
@@ -115,7 +96,6 @@ def delivery(address, truck1, distancesList, truckSpeed, currentTimeTruck1, tota
                                       i.weight,
                                       status)
                     myHash.insert(i.ID, package)
-                    print(i.ID, "   ", i.deliveryDeadline, "   ", i.address)
                     truck1.remove(i)
                     break
             temp = addressIndexTruck1.index(nextIndexForDistances)
@@ -125,14 +105,6 @@ def delivery(address, truck1, distancesList, truckSpeed, currentTimeTruck1, tota
         moving = moving + 1
 
     distBackToHub = distancesList[0][backToHub] # Get the distance from the last address back to the hub
-    '''
-    totalMiles = totalMiles + distBackToHub  # Add the distance to totalMiles
-    # Calculate the time to travel that distance and add it to the current time
-    minutes = math.floor(distBackToHub / truckSpeed)
-    seconds = round(((distBackToHub / truckSpeed) - (
-        math.floor((distBackToHub / truckSpeed)))) * 60)
-    currentTimeTruck1 = currentTimeTruck1 + datetime.timedelta(minutes=minutes, seconds=seconds)
-    '''
     returnValues = list()
     returnValues.append(totalMiles)
     returnValues.append(currentTimeTruck1)
